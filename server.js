2245d8545c50562278cd772005f818f6d76b35eb
Hitesh Mujwani <145272517+hiteshmujwani@users.noreply.github.com>
2024-02-14 12:51:53 +0530

    cors error fix

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 import express from "express";
 import cors from "cors";
 import authRoute from "./routes/authRoute.js";
+import path from "path";
+import { fileURLToPath } from "url";
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "./client/public")));
+
+// app.use(
+//   cors({
+//     origin: "https://new-ecom.vercel.app/",
+//     methods: ["GET", "POST"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//     exposedHeaders: ["Content-Length"],
+//     credentials: true,
+//   })
+// );
 
 app.get("/", (req, res) => {
   res.send("hello from hitesh");
