17f0c80fe1bbc8e82ba90e89ff4ab6b9cc1edb87
Hitesh Mujwani <145272517+hiteshmujwani@users.noreply.github.com>
2024-02-15 20:15:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "./client/public")));
+app.use(express.static(path.join(__dirname, "./client/public/index.html")));
 
 
 app.use((req, res, next) => {
